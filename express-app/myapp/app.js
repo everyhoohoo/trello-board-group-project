525bb317f3c022ec7cac45d42337a2560bd6b71f
RYUK\Apples <36172664+everyhoohoo@users.noreply.github.com>
2018-03-27 22:59:41 -0500

    Added simple working Auth0 login to express. Only run express when testing. Needs logout, restifyand sequelize functionality with users.

--- express-app/myapp/app.js
+++ express-app/myapp/app.js
@@ -4,11 +4,42 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var trello = require('./routes/trello');
+dotenv.load();
 
+
+const index = require('./routes/index');
+const user = require('./routes/user');
+//const trello = require('./routes/trello');
+
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: 'agilesharkboard.auth0.com',
+    clientID: 'x6cdAQRT0ecZm93G8aEoXvXwmorejhSk',
+    clientSecret: 'jW8pcDi7-hMnJUvboE9bhROwDlcSUZ1GgoxO-E9Z3kqb6DWkanvJ8chzy7hlM36k',
+    callbackURL: 'http://localhost:3000/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 var app = express();
 
@@ -22,11 +53,42 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', trello);
-app.use('/users', users);
-app.use('/trello', trello);
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function(req, res, next) {
+ if (req && req.query && req.query.error) {
+   req.flash("error", req.query.error);
+ }
+ if (req && req.query && req.query.error_description) {
+   req.flash("error_description", req.query.error_description);
+ }
+ next();
+});
+
+// Check logged in
+app.use(function(req, res, next) {
+  res.locals.loggedIn = false;
+  if (req.session.passport && typeof req.session.passport.user != 'undefined') {
+    res.locals.loggedIn = true;
+  }
+  next();
+});
+
+app.use('/', index);
+app.use('/user', user);
+//app.use('/trello', trello);
 
 
 // catch 404 and forward to error handler
@@ -36,15 +98,28 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
--- express-app/myapp/routes/index.js
+++ express-app/myapp/routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+const env = {
+  AUTH0_CLIENT_ID: 'x6cdAQRT0ecZm93G8aEoXvXwmorejhSk',
+  AUTH0_DOMAIN: 'agilesharkboard.auth0.com',
+  AUTH0_CALLBACK_URL: 'http://localhost:3000/callback'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', passport.authenticate('auth0', {
+  clientID: env.AUTH0_CLIENT_ID,
+  domain: env.AUTH0_DOMAIN,
+  redirectUri: env.AUTH0_CALLBACK_URL,
+  responseType: 'code',
+  audience: 'https://' + env.AUTH0_DOMAIN + '/userinfo',
+  scope: 'openid profile'}),
+  function(req, res) {
+    res.redirect("/");
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/callback',
+  passport.authenticate('auth0', {
+    failureRedirect: '/failure'
+  }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  }
+);
+
+router.get('/failure', function(req, res) {
+  var error = req.flash("error");
+  var error_description = req.flash("error_description");
+  req.logout();
+  res.render('failure', {
+    error: error[0],
+    error_description: error_description[0],
+  });
 });
 
 module.exports = router;
